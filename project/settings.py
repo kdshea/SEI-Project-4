b455ed1de43a504d194566a851202c9ada7f56fb
Kate Shea <daishea@gmail.com>
2022-09-20 20:39:53 +0530

    Turned off debug'

--- project/settings.py
+++ project/settings.py
@@ -32,10 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRETY_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
