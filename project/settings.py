2339df91c483027b3a181843ad7851073f9e5aa6
Kate Shea <daishea@gmail.com>
2023-02-02 20:42:01 +0530

    Build.sh and other changes to deploy to Render

--- project/settings.py
+++ project/settings.py
@@ -13,15 +13,15 @@ import django_on_heroku
 from pathlib import Path
 import os
 import environ
-
-from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -29,12 +29,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-wi3_q4)yoh^l07bi@njmsm4tds*-2+3ai8aissn3f^r-hhb1tj'
 
-SECRETY_KEY = env('SECRET_KEY')
+# SECRETY_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = '*'
 
 # Application definition
 
@@ -92,13 +95,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'job-api',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+# }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'job-api',
-        'HOST': 'localhost',
-        'PORT': 5432
-}
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 REST_FRAMEWORK = {
