60371ecf912d7f2eb2584ffbdc41b0280bbf0604
Kate Shea <daishea@gmail.com>
2023-02-03 13:59:11 +0530

    Update settings.py
    Updated static file settings

--- project/settings.py
+++ project/settings.py
@@ -157,7 +157,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ROOT_URLCONF = 'project.urls' #check if you have this already, if not add it in
 
 STATIC_URL = '/static/' # same with this
-
+STATIC_ROOT = BASE_DIR / "static" 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'client', "build", "static"),
 )
